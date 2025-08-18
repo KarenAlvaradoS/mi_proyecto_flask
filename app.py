b69941e94ee814ef6c6814cdc55c971947ef669f
Estefania AS <ek.alvarados@uea.edu.ec>
2025-08-18 02:43:58 -0500

    Agregado saludo dinámico para cualquier usuario

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return render_template('usuario.html', nombre=nombre)
+    return render_template(f'usuario/{nombre}.html', nombre=nombre)
 
 if __name__ == '__main__':
     app.run(debug=True)
